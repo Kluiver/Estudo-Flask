d6789b08e223996019fa5f0b089468b89c9a697d
Kluiver Araujo <kluiverdev@gmail.com>
2025-02-13 15:15:37 -0300

    Segundo commit - Trabalhando com dados do back para o front

--- app/views.py
+++ app/views.py
@@ -2,11 +2,27 @@
 from app import app
 
 #importando render template, arquivo responsável por renderizar o arquivo HTML
-from flask import render_template
+# URL_FOR é para direcionar links
+from flask import render_template, url_for
 
 #criando a rota para o aplicativo
 #colocando a barra sem nada para ser a página inicial
 @app.route('/')
 #função que vai renderizar a pagina
 def homepage():
-    return render_template('index.html')
+    #Informaçoes do backend para o front end
+    usuario = 'Kluiver'
+    idade = 24
+
+    #salvando as informações do backend em um dicionário para passar mais facilmente para o front-end
+    context {
+        'usuario': usuario,
+        'idade': idade
+    }
+    return render_template('index.html', context=context) #Passando o dicionário com as informaçoes para o front
+
+
+#criando outra rota
+@app.route('/nova')
+def nova_pagina():
+    return 'Nova página.'
