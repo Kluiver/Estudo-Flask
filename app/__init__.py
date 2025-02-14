310526846db21c3468e03a19a37db950a591d9ff
Kluiver Araujo <kluiverdev@gmail.com>
2025-02-14 13:43:09 -0300

    Adicionado e configurado o banco de dados

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,25 @@
 # Importando a classe Flask
 from flask import Flask, request
 
+# Importando a ferramenta responsável pelo banco de dados
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 #startando o app
 app = Flask(__name__) # Pegando o nome para o aplicativo do arquivo que estou trabalhando
+# Configurando o banco de dados
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' #define qual o caminho para o banco de dados do nosso sistema
+# Desativando o check automático de modificações
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Criando o banco de dados
+db = SQLAlchemy(app)
+# Definindo o aplicativo de migração passando o app e o banco de dados criado
+migrate = Migrate(app, db)
+
+# PARA CRIAR O BANCO DE DADOS, RODA NO TERMINAL "flask db init"
+# APÓS ISSO, RODAR O COMANDO "flask db --message migrate '<mensagem de commit>' "
+# EM SEGUIDA, RODAR O COMANDO "flask db upgrade" PARA QUE AS ALTERAÇOES SUBAM PARA O BANCO
 
 from app.views import homepage
+from app.models import Contato
---
+++ app/models.py
@@ -0,0 +1,16 @@
+from app import db
+#importando date de datetime
+from datetime import datetime, timezone
+
+# Criando a classe contato reponsável por criar a tabela no banco de dados
+class Contato(db.Model): #TODA classe vai herdar o modelo do banco de dados
+    
+    # TODA tabela precisa de um ID
+    id = db.Column(db.Integer, primary_key= True) # Define essa tabela como principal
+    # Definindo os dados que preciso para essa classe
+    nome = db.Column(db.String, nullable= True) #String, não pode ser nulo
+    data_envio = db.Column(db.DateTime, default= datetime.now(timezone.utc)) #definindo a data atual sendo a data atual
+    email = db.Column(db.String, nullable= True)
+    assunto = db.Column(db.String, nullable= True)
+    mensagem = db.Column(db.String, nullable= True)
+    respodida = db.Column(db.Integer, default= 0)
