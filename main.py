da5590d1ba858f7b12b4f88c8837c0a4f7232ceb
Kluiver Araujo <kluiverdev@gmail.com>
2025-02-20 12:16:08 -0300

    Removido o ambiente de testes

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ from app import app
 #verificando se estou no programa principal
 if __name__ == '__main__':
     # rodando o aplicativo em debug mode
-    app.run(host='0.0.0.0', port='5555', debug=True)
+    app.run()
